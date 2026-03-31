5202d6fc027bcb38ad73abbf064bbf9e9c65b1ef
苏洋 <1493367306@qq.com>
2026-03-31 19:56:50 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,9 @@
 export const SITE_TITLE = "王苏洋Blog";
-export const AUTHOR = 'coderfee';
+export const AUTHOR = '王苏洋';
 export const SITE_DESCRIPTION =
-  "coderfee's blog 是一个关于编程、AI 应用、读书笔记和个人随笔的博客。一起探索编程之美、AI 之智、读书之乐和写作之趣吧！";
+  "王苏洋的博客，专注于编程、AI 应用、读书笔记和个人随笔，分享技术与生活思考。";
 
-export const EMAIL_ENCODED = 'Y29kZXJmZWVAb3V0bG9vay5jb20=';
+export const EMAIL_ENCODED = 'MTQ5MzM2NzMwNkBxcC5jb20=';
 
 export const FOLO_CHALLENGE = `
   <follow_challenge>
